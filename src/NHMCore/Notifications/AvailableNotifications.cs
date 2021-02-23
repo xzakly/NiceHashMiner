f790732072170e32985e5cb2f1a65161f662ffd6
luc1an24 <domen.kirnkrefl@nicehash.com>
2021-02-23 10:44:09 +0100

    add copy to clipboard action to notifications - log report id

--- src/NHMCore/Notifications/AvailableNotifications.cs
+++ src/NHMCore/Notifications/AvailableNotifications.cs
@@ -4,6 +4,7 @@ using NHMCore.Utils;
 using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Windows.Forms;
 using static NHMCore.Translations;
 
 namespace NHMCore.Notifications
@@ -347,6 +348,12 @@ namespace NHMCore.Notifications
             var sentence = Tr("was uploaded.");
             if (!success) sentence = Tr("was not uploaded. Please contact our support team for help.");
             var notification = new Notification(NotificationsType.Info, NotificationsGroup.LogArchiveUpload, Tr("Log archive upload result"), Tr("The log archive with the following ID: {0}", uuid) + " " + sentence);
+
+            notification.Action = new NotificationAction
+            {
+                Info = Tr("Copy ID"),
+                Action = () => { Clipboard.SetText(uuid); }
+            };
             NotificationsManager.Instance.AddNotificationToList(notification);
         }
 
